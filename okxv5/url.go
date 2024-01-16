75685e0245f48cab90d4d2869f17dbec0a6e4df9
msw <mswl@ya.ru>
2024-01-16 11:31:27 +0300

    use 443 port for ws

--- okxv5/url.go
+++ okxv5/url.go
@@ -4,5 +4,5 @@ const (
 	MainBaseUrl  = "https://aws.okx.com"
 	ApiVersion   = "api/v5"
 	GinAreaTag   = "557f6da2e9c0BCDE"
-	WebsocketUrl = "wss://wsaws.okx.com:8443/ws"
+	WebsocketUrl = "wss://wsaws.okx.com:443/ws"
 )
