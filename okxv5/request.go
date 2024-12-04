681bd4ea9ea327edd47a10a7f096601e32507acf
msw <mswl@ya.ru>
2024-12-04 18:30:53 +0300

    fix 504 status gateway timeout

--- okxv5/request.go
+++ okxv5/request.go
@@ -61,9 +61,10 @@ func req[R, T any](c *Client, method string, path string, request any, transform
 	h := perf.Do()
 	if h.Error == nil {
 		r.StatusCode = h.StatusCode
-		if h.BodyExists() && r.StatusCode != http.StatusServiceUnavailable {
+		if h.BodyExists() && r.StatusCode != http.StatusServiceUnavailable && r.StatusCode != http.StatusGatewayTimeout {
 			// 503 Service Unavailable response-body: {"message":"name resolution failed"}
 			// 503 Service Unavailable response-body: {"code":"50001","data":[],"inTime":"1731058879657762","msg":"Service temporarily unavailable. Please try again later ","outTime":"1731058879658004"}
+			// 504 Gateway Timeout response-body: {"message":"The upstream server is timing out", "error_id":"dd797d106886196dad573f3de12f0300"}
 			raw := new(response[R])
 			// check that the server response is json
 			r.Error = h.Json(raw) //json parsing
