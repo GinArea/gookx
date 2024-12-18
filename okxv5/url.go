11259648e6fee7e4871c25011ba5955ed51598cc
Oleg Kuzmenko <>
2024-12-18 13:57:29 +0300

    tag updated

--- okxv5/url.go
+++ okxv5/url.go
@@ -3,6 +3,6 @@ package okxv5
 const (
 	MainBaseUrl  = "https://aws.okx.com"
 	ApiVersion   = "api/v5"
-	GinAreaTag   = "557f6da2e9c0BCDE"
+	GinAreaTag   = "2f7f28d58594BCDE"
 	WebsocketUrl = "wss://wsaws.okx.com:443/ws"
 )
