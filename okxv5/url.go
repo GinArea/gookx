78c91cfaee1081cdb6bc673dc905bb0ffe3f83ba
msw <mswl@ya.ru>
2025-04-16 13:26:06 +0300

    update url

--- okxv5/url.go
+++ okxv5/url.go
@@ -1,7 +1,7 @@
 package okxv5
 
 const (
-	MainBaseUrl  = "https://okx.com"
+	MainBaseUrl  = "https://www.okx.com"
 	ApiVersion   = "api/v5"
 	GinAreaTag   = "2f7f28d58594BCDE"
 	WebsocketUrl = "wss://ws.okx.com:443/ws"
