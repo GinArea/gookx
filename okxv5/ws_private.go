3f943616e814cc0a03592ac94d779a54efd101a9
msw <mswl@ya.ru>
2025-11-28 17:10:37 +0300

    fix WithOnDialError

--- okxv5/ws_private.go
+++ okxv5/ws_private.go
@@ -94,10 +94,6 @@ func (o *WsPrivate) Run() {
 			o.onDisconnected()
 		}
 	})
-	o.c.WithOnDialError(func(e error) bool {
-		// add delay for reconnect
-		return false
-	})
 	o.c.WithOnResponse(o.onResponse)
 	o.c.WithOnTopic(o.onTopic)
 	o.c.Run()
